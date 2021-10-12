4e314773a2d133c68336a6711d32674698332c36
nandaarizki <nandakyud220801@gmail.com>
2021-10-12 13:01:52 +0700

    challenge 1

---
+++ ChallengeTugas.java
@@ -0,0 +1,28 @@
+public class ChallengeTugas {
+    public static void main(String args[]) {
+        String firstname, lastname, thirdname, addres, hobby, makanankesukaan, phonenumber;
+        int age;
+        double tinggi;
+
+
+        firstname = " Nanda ";
+        lastname = " Rizki ";
+        thirdname = " Fitriyah ";
+        addres = " Sumatera Utara ";
+        hobby = " jogging ";
+        makanankesukaan = " dimsum ";
+        phonenumber = "0812333333";
+        age = 20;
+        tinggi = 152;
+
+        System.out.println("firstname=" + firstname);
+        System.out.println("lastname=" + lastname);
+        System.out.println("thirdname=" + thirdname);
+        System.out.println("addres=" + addres);
+        System.out.println("hobby=" + hobby);
+        System.out.println("makanan kesukaan=" + makanankesukaan);
+        System.out.println("phonenumber=" + phonenumber);
+        System.out.println("age=" + age);
+        System.out.println("tinggi=" + tinggi);
+    }
+} 
